64f3ed047cc064f390f5c1f08bbd64535b35c0a1
LisReyes <launchx10399@innovaccion.mx>
2022-05-08 22:03:10 -0500

    Agregando las pruebas de unidad así como la verificación mediante un index

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const Reader = require ("./../fizzbuz/lib/utils/Reader");
 const ExplorerService = require ("./lib/services/ExplorerService");
+const FizzbuzzService = require("./lib/services/FizzbuzzService");
 const explorers = Reader.readJsonFile("explorer.json");
 // console.log(explorers);
-console.log(ExplorerService.filterByMission(explorers,"node"));
-console.log(ExplorerService.getAmountOfExplorersByMission(explorers,"node"));
-console.log(ExplorerService.getExplorersUsernamesByMission(explorers,"node"));
+// console.log(ExplorerService.filterByMission(explorers,"node"));
+// console.log(ExplorerService.getAmountOfExplorersByMission(explorers,"node"));
+// console.log(ExplorerService.getExplorersUsernamesByMission(explorers,"node"));
+
+const explorer1 = {name: "Explorer1", score: 15}
+console.log(FizzbuzzService.appliValidationInExplorer(explorer1));
