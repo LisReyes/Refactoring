ca764a8ceb610ff829d9752beae05fbd8b89e44b
LisReyes <launchx10399@innovaccion.mx>
2022-05-08 21:31:39 -0500

    Agregando las siguientes separaciones de responsabilidades 1. Obtener el numero de Explorers con Node 2. Obtener el userName

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const Reader = require ("./../fizzbuz/lib/utils/Reader");
-
+const ExplorerService = require ("./lib/services/ExplorerService");
 const explorers = Reader.readJsonFile("explorer.json");
-console.log(explorers);
+// console.log(explorers);
+console.log(ExplorerService.filterByMission(explorers,"node"));
+console.log(ExplorerService.getAmountOfExplorersByMission(explorers,"node"));
+console.log(ExplorerService.getExplorersUsernamesByMission(explorers,"node"));
